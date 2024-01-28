87a1f96a3669866189f56a30188b76467fcd4241
Shuoze Li <szsuzyli@gmail.com>
2024-01-28 14:11:56 -0500

    implement Client for HTTPPool

--- geecache/http.go
+++ geecache/http.go
@@ -1,17 +1,27 @@
 package geecache
 
 import (
+	"MyGeeCache/geecache/consistenthash"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBathPath = "/_geecache"
+const (
+	defaultBathPath = "/_geecache"
+	defaultReplicas = 50
+)
 
 type HTTPPool struct {
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	mu          sync.Mutex
+	peers       *consistenthash.Map
+	httpGetters map[string]*httpGetter
 }
 
 func NewHTTPPool(self string) *HTTPPool {
@@ -54,3 +64,41 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.b)
 }
+
+// httpGetter is a concrete HTTP client implementation for the PeerGetter interface.
+// It is responsible for fetching data from a remote peer using HTTP.
+type httpGetter struct {
+	baseURL string
+}
+
+// Get is the implementation of the PeerGetter interface for httpGetter.
+// It sends an HTTP GET request to the specified remote node and retrieves the data associated with the given group and key.
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Println("Close error")
+		}
+	}(res.Body)
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+	return bytes, nil
+}
+
+// Ensure that httpGetter implements the PeerGetter interface.
+var _ PeerGetter = (*httpGetter)(nil)
